0ad9aa783d56a32af473275d8709239e0778100c
AndrewDobb <andrewdobb@gmail.com>
2018-02-28 11:26:09 +0000

    Updated Data Helper and Constants

--- Core/Constants.cs
+++ Core/Constants.cs
@@ -1,9 +1,17 @@
-﻿using System;
+﻿using System.Configuration;
 
 namespace Core
 {
     public class Constants
     {
         public static string Constant = "Here's a string";
+        public static string DateFormat = "";
+        public static int CachingInMinutes
+        {
+            get
+            {
+                return Helpers.DataHelper.TryConvertInt(ConfigurationManager.AppSettings["CachingInMinutes"]);
+            }
+        }
     }
 }
---
+++ Core/Helpers/DataHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Helpers
+{
+    public class DataHelper
+    {
+        public static int TryConvertInt(string input)
+        {
+            try
+            {
+                return Convert.ToInt32(input);
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+                return 0;
+            }
+        }
+        public static void LogError(Exception ex)
+        {
+            //to-do: log error
+            throw ex;
+        }
+    }
+}
